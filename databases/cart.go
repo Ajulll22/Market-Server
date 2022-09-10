29cb5f67bbc3d02319edcf4311a36b84d9a84208
Ajulll22 <ajulrizki@gmail.com>
2022-09-10 14:13:49 +0700

    database migrasi

---
+++ databases/cart.go
@@ -0,0 +1,11 @@
+package databases
+
+type Cart struct {
+	Id_cart     int    `gorm:"primaryKey;type:int(11)"`
+	Jumlah      int    `gorm:"type:int(11)"`
+	Total_harga int    `gorm:"type:int(50)"`
+	Keterangan  string `gorm:"type:varchar(75)"`
+	Status_cart bool   `gorm:"default:true"`
+	Id_product  int
+	Id_user     int
+}
