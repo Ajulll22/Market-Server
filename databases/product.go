db387dc923376070cfc5f957fc4f6afbb4fe391d
Ajulll22 <ajulrizki@gmail.com>
2022-09-10 14:14:13 +0700

    database migrasi

---
+++ databases/product.go
@@ -0,0 +1,13 @@
+package databases
+
+type Product struct {
+	Id_product        int    `gorm:"primaryKey;type:int(11)"`
+	Nama_product      string `gorm:"type:varchar(50)"`
+	Deskripsi_product string `gorm:"type:text"`
+	Gambar_product    string `gorm:"type:varchar(50)"`
+	Url_product       string `gorm:"type:varchar(50)"`
+	Status_product    bool   `gorm:"default:true"`
+	Id_category       int
+	Cart              []Cart     `gorm:"foreignKey:Id_product;references:Id_product;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Trx_item          []Trx_item `gorm:"foreignKey:Id_product;references:Id_product;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
