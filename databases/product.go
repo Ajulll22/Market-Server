d53258564145e299a26773d1534d76dddc865cf3
Ajulll22 <ajulrizki@gmail.com>
2022-09-14 18:46:36 +0700

    product db

--- databases/product.go
+++ databases/product.go
@@ -5,7 +5,8 @@ type Product struct {
 	Nama_product      string `gorm:"type:varchar(50)"`
 	Deskripsi_product string `gorm:"type:text"`
 	Gambar_product    string `gorm:"type:varchar(50)"`
-	Url_product       string `gorm:"type:varchar(50)"`
+	Harga_product     int    `gorm:"type:int(50)"`
+	Url_product       string `gorm:"type:varchar(100)"`
 	Status_product    bool   `gorm:"default:true"`
 	Id_category       int
 	Cart              []Cart     `gorm:"foreignKey:Id_product;references:Id_product;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
