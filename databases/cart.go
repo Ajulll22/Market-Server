ebff00e5e9fbca31ed8438b88309ae5bd4b2be54
Ajulll22 <ajulrizki@gmail.com>
2022-09-14 18:46:30 +0700

    cart db

--- databases/cart.go
+++ databases/cart.go
@@ -3,7 +3,7 @@ package databases
 type Cart struct {
 	Id_cart     int    `gorm:"primaryKey;type:int(11)"`
 	Jumlah      int    `gorm:"type:int(11)"`
-	Total_harga int    `gorm:"type:int(50)"`
+	Total_harga int    `gorm:"type:int(100)"`
 	Keterangan  string `gorm:"type:varchar(75)"`
 	Status_cart bool   `gorm:"default:true"`
 	Id_product  int
