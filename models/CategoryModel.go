60180fef682e432075d352346688cb717e561ad5
Ajulll22 <ajulrizki@gmail.com>
2022-09-14 18:47:16 +0700

    category model

--- models/CategoryModel.go
+++ models/CategoryModel.go
@@ -1,6 +1,6 @@
 package models
 
 type Category struct {
-	Id_category   int    `json:"id_category,omitempty"`
+	Id_category   int    `json:"id_category,omitempty" gorm:"primaryKey"`
 	Nama_category string `json:"nama_category,omitempty"`
 }
