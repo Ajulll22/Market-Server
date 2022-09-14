1486dd2ba75d8831b35b5ed52efda923398e81c5
Ajulll22 <ajulrizki@gmail.com>
2022-09-14 18:46:48 +0700

    transaction db

--- databases/transaction.go
+++ databases/transaction.go
@@ -2,7 +2,7 @@ package databases
 
 type Transaction struct {
 	Id_trx    int `gorm:"primaryKey;type:int(11)"`
-	Total_trx int `gorm:"type:int(50)"`
+	Total_trx int `gorm:"type:int(100)"`
 	Id_user   int
 	Trx_item  []Trx_item `gorm:"foreignKey:Id_trx;references:Id_trx;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
