03d30948d39d789000983c3bc65d3a719f87fe03
Ajulll22 <ajulrizki@gmail.com>
2022-09-14 18:48:34 +0700

    main

--- main.go
+++ main.go
@@ -19,7 +19,13 @@ func main() {
 	}
 
 	app := fiber.New()
-	app.Use(cors.New())
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+	app.Static(
+		"/images",
+		"./public/images",
+	)
 
 	databases.Conect(c.DBUrl)
 
