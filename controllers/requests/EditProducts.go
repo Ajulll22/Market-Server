bb085595652b5a7885b4936915a6e26a2ce4b4db
Ajulll22 <ajulrizki@gmail.com>
2022-10-02 01:09:41 +0700

    .

---
+++ controllers/requests/EditProducts.go
@@ -0,0 +1,10 @@
+package requests
+
+import "encoding/json"
+
+type EditProductRequest struct {
+	Nama_product      string      `form:"nama_product" validate:"required"`
+	Deskripsi_product string      `form:"deskripsi_product" validate:"required"`
+	Id_category       json.Number `form:"id_category" validate:"required,number"`
+	Harga_product     json.Number `form:"harga_product" validate:"required,number"`
+}
