1adcfb0f98e2edcb6ef6966ae5293a98c890d2f3
Ajulll22 <ajulrizki@gmail.com>
2022-10-02 01:09:13 +0700

    .

--- databases/transaction.go
+++ databases/transaction.go
@@ -1,8 +1,13 @@
 package databases
 
+import "time"
+
 type Transaction struct {
-	Id_trx    int `gorm:"primaryKey;type:int(11)"`
-	Total_trx int `gorm:"type:int(100)"`
-	Id_user   int
-	Trx_item  []Trx_item `gorm:"foreignKey:Id_trx;references:Id_trx;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Id_trx     int    `gorm:"primaryKey;type:int(11)"`
+	Total_trx  int    `gorm:"type:int(100)"`
+	Alamat_trx string `gorm:"type:varchar(100)"`
+	Status_trx int    `gorm:"type:int(5)"`
+	Created_at time.Time
+	Id_user    int
+	Trx_item   []Trx_item `gorm:"foreignKey:Id_trx;references:Id_trx;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
