4ae7cd885eebf6a0ac75e6dbfeff91d50fa1dfee
Ajulll22 <ajulrizki@gmail.com>
2022-10-02 01:09:16 +0700

    .

--- databases/trx_item.go
+++ databases/trx_item.go
@@ -1,10 +1,9 @@
 package databases
 
 type Trx_item struct {
-	Id_item         int    `gorm:"primaryKey;type:int(11)"`
-	Jumlah_item     int    `gorm:"type:int(11)"`
-	Keterangan_item string `gorm:"type:varchar(75)"`
-	Harga_item      int    `gorm:"type:int(50)"`
-	Id_product      int
-	Id_trx          int
+	Id_item     int `gorm:"primaryKey;type:int(11)"`
+	Jumlah_item int `gorm:"type:int(11)"`
+	Harga_item  int `gorm:"type:int(50)"`
+	Id_product  int
+	Id_trx      int
 }
