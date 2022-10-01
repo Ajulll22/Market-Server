1249f7fba45df64e8d7992f494745be2ce0956f9
Ajulll22 <ajulrizki@gmail.com>
2022-10-02 01:09:19 +0700

    .

--- databases/user.go
+++ databases/user.go
@@ -5,7 +5,9 @@ type User struct {
 	Nama_user     string        `gorm:"type:varchar(50)"`
 	Email_user    string        `gorm:"type:varchar(50)"`
 	Password_user string        `gorm:"type:varchar(100)"`
+	Alamat_user   string        `gorm:"type:varchar(100)"`
 	Level_user    int           `gorm:"type:varchar(5);default:1"`
+	Status_user   int           `gorm:"type:int(5);default:1"`
 	Cart          []Cart        `gorm:"foreignKey:Id_user;references:Id_user;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Transaction   []Transaction `gorm:"foreignKey:Id_user;references:Id_user;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
