2f304556fb90bea4707160f3139ba0e61b27569b
Ajulll22 <ajulrizki@gmail.com>
2022-10-02 01:09:22 +0700

    .

--- models/CartModel.go
+++ models/CartModel.go
@@ -3,9 +3,7 @@ package models
 type Cart struct {
 	Id_cart     int     `json:"id_cart,omitempty" gorm:"primaryKey"`
 	Jumlah      int     `json:"jumlah,omitempty"`
-	Total_harga int     `json:"total_harga,omitempty"`
-	Keterangan  string  `json:"keterangan,omitempty"`
-	Status_cart bool    `json:"status_cart,omitempty"`
+	Total_harga int     `json:"total_harga"`
 	Id_product  int     `json:"id_product,omitempty"`
 	Id_user     int     `json:"id_user,omitempty"`
 	Product     Product `gorm:"foreignKey:Id_product;references:Id_product;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"product,omitempty"`
