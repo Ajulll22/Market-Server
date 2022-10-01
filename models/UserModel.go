10e145485f4fe0ae9dfd010c5d623983a6e20a10
Ajulll22 <ajulrizki@gmail.com>
2022-10-02 01:09:32 +0700

    .

--- models/UserModel.go
+++ models/UserModel.go
@@ -4,6 +4,8 @@ type User struct {
 	Id_user       int    `json:"id_user,omitempty" gorm:"primaryKey"`
 	Nama_user     string `json:"nama_user,omitempty"`
 	Email_user    string `json:"email_user,omitempty"`
-	Level_user    int    `json:"level_user,omitempty"`
+	Alamat_user   string `json:"alamat_user,omitempty"`
+	Level_user    int    `json:"level_user,omitempty" gorm:"default:1"`
+	Status_user   int    `json:"status_user,omitempty" gorm:"default:1"`
 	Password_user string `json:"-"`
 }
